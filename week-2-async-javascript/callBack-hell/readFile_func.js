a97016b9bf8947b197ea3392d8cc8e85d57e50ff
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-05-08 23:53:25 +0530

    Create readFile_func.js

---
+++ week-2-async-javascript/callBack-hell/readFile_func.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+function readFilePromisified(filePath) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, "utf-8", function (err, data) {
+      if (err) {
+        reject("Error while reading file");
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+function onDone(data) {
+  console.log(data);
+}
+
+function onError(err) {
+  console.log("Error: " + err);
+}
+
+readFilePromisified("abc.txt").then(onDone).catch(onError);
