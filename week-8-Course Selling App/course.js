53490de423ab6e1af069fb5357e69ed42570ca1c
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-05-11 23:39:57 +0530

    Add course routes for purchase and preview

---
+++ week-8-Course Selling App/course.js
@@ -0,0 +1,12 @@
+function createCourseRoutes(app){
+    
+    app.post("/course/purchase", function(req,res){
+    })
+
+    app.get("/course/preview", function(req,res){
+    })
+}
+
+module.exports = {
+    createCourseRoutes : createCourseRoutes
+}
