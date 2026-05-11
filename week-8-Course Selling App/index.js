d17b77018dff7f78f9930a22a5fa28f81a26bf4b
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-05-11 23:36:47 +0530

    Add files via upload

--- week-8-Course Selling App/index.js
+++ week-8-Course Selling App/index.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const app = express();
 
+app.use(express());
+
+app.post("/login", function(req,res){
+
+})
+
+app.post("/signup", function(req,res){
+
+})
+
+app.post("/signup", function(req,res){
+
+})
+
+app.post("/signup", function(req,res){
+
+})
+
+app.listen(3000);
