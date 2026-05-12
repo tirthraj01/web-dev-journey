74d4412a9e60a50049361b225453c0c4414cd583
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-05-12 23:33:08 +0530

    Create admin.js

---
+++ week-8-Course Selling App/routes/admin.js
@@ -0,0 +1,24 @@
+const {Router} = require("express");
+
+const adminRouter = Router();
+
+adminRouter.post("/signup", function(req,res){
+    res.json({
+        message: " signup endpoint"
+})
+
+})
+
+adminRouter.post("/signin", function(req,res){
+    res.json({
+        message: " signin endpoint"
+    })
+
+})
+
+adminRouter.post("/course", function(req,res){
+    res.json({
+        message: " signin endpoint"
+    })
+
+})
