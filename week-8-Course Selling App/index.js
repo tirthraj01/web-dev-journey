7c14a7ff1970b57c261bab8bfc1e5cced581e8e9
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-05-15 23:27:59 +0530

    Update index.js
    Refactor server setup to use routers and connect to MongoDB.

--- week-8-Course Selling App/index.js
+++ week-8-Course Selling App/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const { createUserRoutes} = require("./user");
-const { createCourseRoutes} = require("./course");
+const mongoose = require("mongoose");
+const { userRouter} = require("./routes/user");
+const { courseRouter} = require("./routes/course");
+const { adminRouter} = require("./routes/admin")
 //Routing in express the express router 
 const app = express();
 
-createUserRoutes(app);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/admin", adminRouter)
 
-app.use(express());
+async function main() {
+    mongoose.connect("mongodb+srv://Tirthraj01:Y8yo3ELsOmlKnFht@cluster0.thsrevp.mongodb.net/");
+    app.listen(3000);
+    console.log(" listening to port ")
+}
 
-
-app.listen(3000);
+main()
