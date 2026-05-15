dc0f846cf546c079aa9e8b95d6405becae86dfde
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-05-15 23:29:14 +0530

    Update ds.js

--- week-8-Course Selling App/ds.js
+++ week-8-Course Selling App/ds.js
@@ -1,23 +1,47 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
-const ObjectId = mongoose.ObjectId;
+const ObjectId = mongoose.Types.ObjectId;
 
-const User = new Schema({
-  name: String,
-  email: {type: String, unique:true },
-  password: String
+
+const userSchema = new Schema({
+
+    email: { type: String, unique:true },
+    password: String,
+    firstName: String,
+    lastName: String 
 });
 
-const Todo = new Schema({
-    userId: ObjectId,
+const adminSchema = new Schema({
+    email: { type: String, unique:true },
+    password: String,
+    firstName: String,
+    lastName: String 
+
+});
+
+const courseSchema = new Schema({
     title: String,
-    done: Boolean
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+
 });
 
-const UserModel = mongoose.model('users', User);
-const TodoModel = mongoose.model('todos', Todo);
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("user", adminSchema);
+const courseModel = mongoose.model("user", courseSchema);
+const purchaseModel = mongoose.model("user", purchaseSchema);
 
 module.exports = {
-    UserModel: UserModel,
-    TodoModel: TodoModel
+    userModel,
+    courseModel,
+    adminModel,
+    purchaseModel
 }
