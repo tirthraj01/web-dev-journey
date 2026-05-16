fee6d468cccd8a678419387d865eca9262b07d98
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-05-16 23:44:00 +0530

    Update index.js
    Refactor index.js to use dotenv for MongoDB connection and improve structure.

--- week-8-Course Selling App/index.js
+++ week-8-Course Selling App/index.js
@@ -1,19 +1,23 @@
-const express = require('express');
+require('dotenv').config()
+console.log(process.env.MONGO_URL)
+const express = require("express");
 const mongoose = require("mongoose");
-const { userRouter} = require("./routes/user");
-const { courseRouter} = require("./routes/course");
-const { adminRouter} = require("./routes/admin")
-//Routing in express the express router 
+
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 const app = express();
+app.use(express.json());
+
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
-app.use("/api/v1/admin", adminRouter)
 
 async function main() {
-    mongoose.connect("mongodb+srv://Tirthraj01:Y8yo3ELsOmlKnFht@cluster0.thsrevp.mongodb.net/");
+    await mongoose.connect(process.env.MONGO_URL)
     app.listen(3000);
-    console.log(" listening to port ")
+    console.log("listening on port 3000")
 }
 
 main()
