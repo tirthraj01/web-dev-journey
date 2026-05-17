cbf10608cb4766497d439932f2c39a67b75fc387
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-05-17 16:33:22 +0530

    Update course.js

--- week-8-Course Selling App/routes/course.js
+++ week-8-Course Selling App/routes/course.js
@@ -1,21 +1,32 @@
-const {Router} = require("express");
-
+const { Router } = require("express");
+const { userMiddleware } = require("../middleware/user");
+const { purchaseModel, courseModel } = require("../db")
 const courseRouter = Router();
 
-courseRouter.post("/purchase", function(req,res){
+courseRouter.post("/purchase", userMiddleware, async function(req, res) {
+    const userId = req.userId;
+    const courseId = req.body.courseId;
+
+    // should check that the user has actually paid the price
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
+
     res.json({
-        message: " signin endpoint"
+        message: "You have successfully bought the course"
     })
 })
 
-courseRouter.get("/preview", function(req,res){
+courseRouter.get("/preview", async function(req, res) {
+    
+    const courses = await courseModel.find({});
+
     res.json({
-        message: " signin endpoint"
+        courses
     })
-
 })
-
 
 module.exports = {
-    courseRouter : courseRouter
+    courseRouter: courseRouter
 }
