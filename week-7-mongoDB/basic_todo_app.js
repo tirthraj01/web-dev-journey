3371da2c48529bb0097e3cd2bb82592b056b9241
Tirthraj Chavan <tirthrajchavan07@gmail.com>
2026-06-19 20:43:38 +0530

    correct form of todo app

--- week-7-mongoDB/basic_todo_app.js
+++ week-7-mongoDB/basic_todo_app.js
@@ -1,62 +1,70 @@
-const express = require('express');
+const express = require("express");
 const { UserModel, TodoModel } = require("./db");
-const jwt = require('jsonwebtoken');
-const { default: mongoose } = require('mongoose');
-const JWT_SECRET ="s3cret"
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
-mongoose.connect("")
+const JWT_SECRET = " ";
+
+await mongoose.connect(" ");
 
 const app = express();
 app.use(express.json());
 
-app.post("/signup", async function(req,res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
+app.post("/signup", async function(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    })
+  await UserModel.create({
+    email: email,
+    password: password,
+    name: name
+  });
+
+  res.json({
+    message: "You are logged in"
+  });
+});
+
+app.post("/signin", async function(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await UserModel.findOne({
+    email: email,
+    password: password
+  });
+
+  console.log(user);
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user._id
+      },
+      JWT_SECRET
+    );
+
     res.json({
-        message: "You are logged in!"
-    })
-
+      token: token
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials"
+    });
+  }
 });
 
-app.post("/signin", async function(req,res) {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    const user = await UserModel.findOne({
-        email: email,
-        password: password
-    })
-
-    console.log(user);
-
-    if(user){
-        const token = jwt.sign({
-            id: user._id
-        }, JWT_SECRET );
-        res.json({
-            token: token
-        });
-    } else {
-        res.status(403).json({
-            message: "Incorrect Credentials"
-        });
-    }
-
+app.post("/todo", async function (req, res) {
+    res.json({
+        message: "Todo endpoint"
+    });
 });
 
-app.post("/todo", function(req,res) {
-
-});
-
-app.get("/todos", function(req,res) {
-
+app.get("/todos", async function (req, res) {
+    res.json({
+        message: "Todos endpoint"
+    });
 });
 
 app.listen(3000);
