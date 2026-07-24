598d65e9468ad57d1110c4931b2cf24db20454de
Tirth <tirthrajchavan07@gmail.com>
2026-07-24 20:21:00 +0530

    Create App component for OTP page layout

---
+++ week-13-Tailwind CSS/OtpPage/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import { Otp } from "./components/Otp";
+
+function App() {
+  return (
+    <div  className="h-screen bg-blue-700 flex justify-center items-center">
+      
+      <Otp></Otp>
+    </div>
+  );
+}
+
+export default App;
