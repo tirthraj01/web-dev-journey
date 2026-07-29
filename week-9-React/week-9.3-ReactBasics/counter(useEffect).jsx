9d4e196554fff24dcd386dd9d940517dd8d003ee
Tirth <tirthrajchavan07@gmail.com>
2026-07-29 23:02:03 +0530

    Update counter(useEffect).jsx
    Refactor increaseCount function and useEffect hook to log execution times and manage interval cleanup.

--- week-9-React/week-9.3-ReactBasics/counter(useEffect).jsx
+++ week-9-React/week-9.3-ReactBasics/counter(useEffect).jsx
@@ -1,28 +1,31 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(1)
 
-  function increaseCount(){
-    setCount (function(currentValue){
+  function increaseCount() {
+    setCount(function(currentValue) {
       console.log("how many times it runs " + currentValue);
-      return currentValue+ 1;
+      return currentValue + 1;
     })
   }
 
-  useEffect (function(){
-    console.log("How many times it runs")
-    setInterval(increaseCount,1000);
-  },[])
+  useEffect(function() {
+    console.log("How many times useEffect runs");
 
-  return <div>
-    {count}
-  </div>
+    const interval = setInterval(increaseCount, 1000);
+
+    return function() {
+      clearInterval(interval);
+    };
+  }, [])
+
+  return (
+    <div>
+      {count}
+    </div>
+  )
 }
-
 
 export default App
