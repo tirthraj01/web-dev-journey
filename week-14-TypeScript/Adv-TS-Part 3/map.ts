5746ec8ca7942bcaf0e3a3feb8361232662b40dd
Tirth <tirthrajchavan07@gmail.com>
2026-08-06 21:50:47 +0530

    Implement usersMap using TypeScript Map

---
+++ week-14-TypeScript/Adv-TS-Part 3/map.ts
@@ -0,0 +1,15 @@
+//Map
+interface User4 {
+  id: string;
+  name: string;
+}
+
+// Initialize an empty Map
+const usersMap = new Map<string, User4>();
+
+// Add users to the map using .set
+usersMap.set('abc123', { id: 'abc123', name: 'John Doe' });
+usersMap.set('xyz789', { id: 'xyz789', name: 'Jane Doe' });
+
+// Accessing a value using .get
+console.log(usersMap.get('abc123')); // Output: { id: 'abc123', name: 'John Doe' }
