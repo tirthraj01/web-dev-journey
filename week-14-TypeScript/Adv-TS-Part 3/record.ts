abeaac44b8558a958857ba3326b625024cd61ed4
Tirth <tirthrajchavan07@gmail.com>
2026-08-06 21:49:25 +0530

    Create record.ts

---
+++ week-14-TypeScript/Adv-TS-Part 3/record.ts
@@ -0,0 +1,15 @@
+
+//Record
+interface User3 {
+  id: string;
+  name: string;
+}
+
+type Users = Record<string, User3>;
+
+const users: Users = {
+  'abc123': { id: 'abc123', name: 'John Doe' },
+  'xyz789': { id: 'xyz789', name: 'Jane Doe' },
+};
+
+console.log(users['abc123']); // Output: { id: 'abc123', name: 'John Doe' }
