d192c8a9658f7db96e8299d96b7c0089f9fd5817
Tirth <tirthrajchavan07@gmail.com>
2026-08-11 23:45:33 +0530

    Create todoList.ts

---
+++ week-14-TypeScript/Adv-TS-Part 3/todoList.ts
@@ -0,0 +1,19 @@
+// Todo.tsx
+interface TodoType {
+  title: string;
+  description: string;
+  done: boolean;
+}
+
+interface TodoInput {
+  todo: TodoType;
+}
+
+function Todo({ todo }: TodoInput) {
+  return (
+    <div>
+        <h1>{todo.title}</h1>
+        <h2>{todo.description}</h2>    
+    </div>
+  )
+}
