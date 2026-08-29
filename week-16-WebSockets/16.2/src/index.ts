00e66585d4ab036080b57f554d84cb81b8d37b24
Tirth <tirthrajchavan07@gmail.com>
2026-08-29 16:59:13 +0530

    Implement WebSocket server for chat functionality

---
+++ week-16-WebSockets/16.2/src/index.ts
@@ -0,0 +1,43 @@
+import { WebSocketServer, WebSocket } from "ws"
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface User{
+    socket: WebSocket;
+    room: string;
+}
+
+let allSockets: User[] = [];
+
+wss.on("connection", (socket) => {
+    console.log("user connected");
+
+    socket.on("message", (message) => {
+        // @ts-ignore
+        const parseMessage = JSON.parse(message);
+        if (parseMessage.type === "join") {
+            console.log("user joined room " + parseMessage.payload.roomId);
+            allSockets.push({
+                socket: socket,
+                room: parseMessage.payload.roomId
+            })
+        }
+
+        if (parseMessage.type === "chat") {
+            console.log("user wants to chat");
+
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++){
+                if(allSockets[i]!.socket === socket){
+                    currentUserRoom = allSockets[i]!.room
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++){
+                if(allSockets[i]!.room === currentUserRoom){
+                    allSockets[i]!.socket.send(parseMessage.payload.message)
+                }
+            }
+        }
+    })
+})
