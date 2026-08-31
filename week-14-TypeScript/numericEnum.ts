af9ed8d4328b534b34e0858c2f9280a7f547a6ac
Tirth <tirthrajchavan07@gmail.com>
2026-08-31 23:46:52 +0530

    Add numeric enums for Direction and StatusCode

---
+++ week-14-TypeScript/numericEnum.ts
@@ -0,0 +1,13 @@
+enum Direction {
+  Up,     // 0
+  Down,   // 1
+  Left,   // 2
+  Right   // 3
+}
+
+// Custom starting index
+enum StatusCode {
+  OK = 200,
+  BadRequest,   // 201
+  Unauthorized  // 202
+}
